c7a96f988ec0a3b8004e6d452c1dd9e8c6a2f68d
s1310166 <s.tai16seki07san@icloud.com>
2024-07-02 17:31:21 +0900

    HoT: initial revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -6,10 +6,6 @@ public class HeadsOrTails {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.print("Who are you? \n> ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-
         System.out.println("Tossing a coin...");
 
         int headsCount = 0;
